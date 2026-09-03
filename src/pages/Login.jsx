d7c52577b75b7a953a4dc54fd46d18d70d087b90
rizkyamuktii <rizkyamukti@gmail.com>
2026-09-03 15:07:07 +0700

    cihuy

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { Form, Button, Card, Container, Row, Col, Alert } from "react-bootstrap";
 import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
+import api from "../services/api";
 
 const login = () => {
 
@@ -15,28 +16,33 @@ const login = () => {
         setLoading(true);
 
         try {
-            const response = await fetch('http://localhost:8000/api/login', {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-                body: JSON.stringify({ email, password })
+            const response = await api.post(`/login`, {
+                email,
+                password,
             });
-            const data = await response.json();
-            if (!response.ok) {
-                if (response.status === 422 && data.error) {
-                    const firstKey = Object.keys(data.error)[0];
-                    setError(data.error[firstKey][0]);
-                } else {
-                    setError(data.message || "Upss Email and Password fail");
-                }
-                return;
-            }
-            localStorage.setItem('token', data.token);
+
+
+            localStorage.setItem('token', response.token);
             navigate("/dashboard");
         } catch (error) {
-            console.log(error);
+            console.log(error.response);
+            if (error.response) {
+                if (error.response.status === 422 && error.response.data.error) {
+                    const rawErrors = error.response.data.error;
+                    const formatError = {}
+
+                    Object.keys(rawErrors).forEach((key) => {
+                        formatError[key] = rawErrors[key][0];
+                    });
+
+                    setError(formatError);
+                } else if (error.response.status === 401) {
+                    setError(error.response.data.message || "Please check your email and password!")
+                } else {
+                    //500
+                    setError("Server Error");
+                }
+            }
             setError('Server Error');
         } finally {
             setLoading(false);
@@ -55,15 +61,30 @@ const login = () => {
                             <Form onSubmit={handleLogin}>
                                 <Form.Group className="mb-3">
                                     <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" placeholder="email@gmail.com"
-                                        value={email} onChange={(e) => setEmail(e.target.value)}>
-
-                                    </Form.Control>
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="email@gmail.com"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        isInvalid={!!error?.email}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {error?.email}
+                                    </Form.Control.Feedback>
                                 </Form.Group>
+
                                 <Form.Group className="mb-3">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="enter your password"
-                                        value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="enter your password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        isInvalid={!!error?.password}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {error?.password}
+                                    </Form.Control.Feedback>
                                 </Form.Group>
                                 <button variant="primary" type="submit" className="w-100 py-2 mt-2" disabled={loading}>
                                     {loading ? "Loading..." : "Login"}
